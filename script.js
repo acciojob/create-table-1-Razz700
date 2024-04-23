88e7f8aa30101ad341ab4b12718d7b2bdaef4d3e
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-23 17:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function insert_Row() {
    let table=document.getElementById("sampleTable");
   let newRow=document.createElement("tr");
   newRow.innerHTML="<tr><td>New cell1</td> <td>New cell2</td></tr>";
-  table.append(newRow);
+  table.prepend(newRow);
 }
   
 }
